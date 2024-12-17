3865c09055c09b09fb6e37fac7a23de5d7fdf381
Kishore545 <kishunani92@gmail.com>
2024-12-17 10:24:29 +0530

    seven

--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package pop
 
+import (
+	"fmt"
+)
+
 func Bark() string {
 	return "Woof"
 }
 
 func Barks() string {
 	return "Woof Woof Woof"
+}
+
+func From11() {
+	fmt.Println("I'm from version 1.1.0")
 }
